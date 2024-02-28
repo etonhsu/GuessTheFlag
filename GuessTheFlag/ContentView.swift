549b806833591be4da0b726cd999e739ed1e7196
Eton Hsu <etonhsu111@gmail.com>
2024-02-28 10:44:47 -0800

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,118 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Eton  Hsu on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var currentScore = 0
+    @State private var gameCount = 0
+    @State private var gameEnd = false
+    
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of ")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                            allAttemps()
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score is \(currentScore)")
+                    .foregroundStyle(.white)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(currentScore)")
+        }
+        .alert("Game Over!", isPresented: $gameEnd) {
+            Button("Try again") {
+                reset()
+            }
+        } message: {
+            Text("Your final score was \(currentScore) / 8")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            currentScore += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+        }
+        
+        gameCount += 1
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func allAttemps() {
+        if gameCount == 8 {
+            gameEnd = true
+        }
+    }
+    
+    func reset() {
+        currentScore = 0
+        gameCount = 0
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Eton  Hsu on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
